15957d695f920dca04bd92652d00aab4a9c29670
Ricardo Otálora Luna <ruperto1@protonmail.com>
2022-10-16 17:49:53 -0500

    second

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar'
 
 const Home: NextPage = () => {
   return (
-    <div>
-     <h1>Prometeo</h1>
-    </div>
+    <>
+     <Navbar />
+    </>
   )
 }
 
